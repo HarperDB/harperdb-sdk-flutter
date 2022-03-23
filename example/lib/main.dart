f7f0f3c55cb4b3a76ad95e9ef5d5df4c77ad289b
Colin-Stark <collinscodes@gmail.com>
2022-03-23 20:52:07 +0100

    Lets wing it

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 // This is a necessary file to call our custom functions from the HarperDB package
 import 'package:harperdb/harperdb.dart';
 
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -26,14 +25,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   // This is your Url global variable
-const String HDB_URL = 'https://hdb-publicdemo.harperdbcloud.com';
+  final String hDBURL = 'https://hdb-publicdemo.harperdbcloud.com';
   // This is a global variable for your username
-const String HDB_USER = 'HDB_READER';
+  final String hDBUSER = 'HDB_READER';
   // This is a global variable for your password
-const String HDB_PASSWORD = 'password';
-
+  final String hDBPASSWORD = 'password';
 
   //This handles your return type, it could be either a List for search queries or an Map for queries that insert
   late List harperData;
@@ -45,18 +42,16 @@ const String HDB_PASSWORD = 'password';
   // The return type for the function varies, it can either be a Map or a List depending on the operation you are carrying out
 
   Future //function type is stated as a future
-    <List> // the fucntion will return future in this case but it can also be set to map in other cases, all you have to do is change <List> to <Map<String, dynamic>>
-    loadData() //the function name
-    async  // it has to be asynchronous
+      <List> // the fucntion will return future in this case but it can also be set to map in other cases, all you have to do is change <List> to <Map<String, dynamic>>
+      loadData() //the function name
+  async // it has to be asynchronous
   {
-    var show = await harperDB( //A variable called show is assigned the result ot the function
-      HDB_URL,
-      HDB_USER,
-      HDB_PASSWORD,
-      {
-        "operation": "sql",
-        "sql": "select * from dev.dog"
-      },
+    var show = await harperDB(
+      //A variable called show is assigned the result ot the function
+      hDBURL,
+      hDBUSER,
+      hDBPASSWORD,
+      {"operation": "sql", "sql": "select * from dev.dog"},
     );
     //this shows you if the query ran properly. The result of your search will be shown in the terminal
     debugPrint(
@@ -73,7 +68,6 @@ const String HDB_PASSWORD = 'password';
   //Initialize your function so it runs as the page opens up
   @override
   void initState() {
-
     harperAPI = loadData();
     super.initState();
   }
@@ -81,13 +75,15 @@ const String HDB_PASSWORD = 'password';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(// the widget is used to show data after it has gotten the result
+      body: FutureBuilder(
+        // the widget is used to show data after it has gotten the result
         future: harperAPI,
         builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) { //This block of code states what is to happen while waiting for the result from the database, in this case it is meant to show a circular progress widget
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            //This block of code states what is to happen while waiting for the result from the database, in this case it is meant to show a circular progress widget
             return const Center(child: CircularProgressIndicator());
-          }
-          else if (snapshot.connectionState == ConnectionState.done) { //This block of code explains what to do when it has gotten the data from the database
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            //This block of code explains what to do when it has gotten the data from the database
             return ListView.builder(
               itemCount: harperData.length,
               itemBuilder: (context, index) {
@@ -105,10 +101,9 @@ const String HDB_PASSWORD = 'password';
                 );
               },
             );
-          }
-          else { //This block of code will tell what to do if theres an error
-            return const Center(
-                child: Text("Something went wrong."));
+          } else {
+            //This block of code will tell what to do if theres an error
+            return const Center(child: Text("Something went wrong."));
           }
         },
       ),
--- lib/src/harperconnection.dart
+++ lib/src/harperconnection.dart
@@ -3,24 +3,34 @@ import 'dart:convert';
 // This happpens to be the most important package as it is used to connect to the cloud database via internet
 import 'package:http/http.dart' as http;
 
-//This is a future function as we must await response from the database server 
-Future harperDB( //The name of the function 'harperDB' is what is called in our main.dart or the page where we want to access our database
-  String harperurl, //This is the valid url to your database for example (https://xyz.harperdbcloud.com)
-  String instanceUsername,// this is the username of your instance in flutter
-  String instancePassword,// this is the password the instance you created to store your database.
-  Map<String, dynamic> operation,// This exectues a list of operation you would like to be carried out, examples of operations can be found at https://studio.harperdb.io/resources/examples/NoSQL%20Operations/Search%20By%20Conditions and a video to explain NOSQL AND SQL can be found at https://youtu.be/oJuoddPPVYs
-) async { // The function has to be asynchronous as we are going to be waiting from data from the database
-  String signingDetails = '$instanceUsername:$instancePassword'; //This will combine the username and password together, if for example your username is 'Hello' and your password is 'World'. it will then form 'Hello:World'
+//This is a future function as we must await response from the database server
+Future harperDB(
+  //The name of the function 'harperDB' is what is called in our main.dart or the page where we want to access our database
+  String
+      harperurl, //This is the valid url to your database for example (https://xyz.harperdbcloud.com)
+  String instanceUsername, // this is the username of your instance in flutter
+  String
+      instancePassword, // this is the password the instance you created to store your database.
+  Map<String, dynamic>
+      operation, // This exectues a list of operation you would like to be carried out, examples of operations can be found at https://studio.harperdb.io/resources/examples/NoSQL%20Operations/Search%20By%20Conditions and a video to explain NOSQL AND SQL can be found at https://youtu.be/oJuoddPPVYs
+) async {
+  // The function has to be asynchronous as we are going to be waiting from data from the database
+  String signingDetails =
+      '$instanceUsername:$instancePassword'; //This will combine the username and password together, if for example your username is 'Hello' and your password is 'World'. it will then form 'Hello:World'
   Codec<String, String> stringToBase64 = utf8.fuse(base64);
   String authcode = stringToBase64.encode(signingDetails);
   var url = Uri.parse(harperurl);
-  var response = await http.post( //Here we ar making use of the API details we have provided to authenticate and ask for the data we want to get from our database and we wait for the data then put it in the variable called response
-    url, //First we need the url, This is a variable which links to the harperurl we gave in line 8 
-      headers: <String, String>{ //This section handles the authentication and return type of the data we are sending
+  var response = await http.post(
+      //Here we ar making use of the API details we have provided to authenticate and ask for the data we want to get from our database and we wait for the data then put it in the variable called response
+      url, //First we need the url, This is a variable which links to the harperurl we gave in line 8
+      headers: <String, String>{
+        //This section handles the authentication and return type of the data we are sending
         'Content-Type': 'application/json; charset=UTF-8',
         'authorization': 'Basic $authcode'
       },
-      body: jsonEncode(operation)); //Now here comes the crucial part where we write operation like search for username, insert user data, etc. whatever operations you would perform would be placed in the operations section
-  var result = jsonDecode(response.body); //Now we have gotten the result after waiting for the data, we then decode the data and also put it in a variable called result
-  return result //This is returned whenever the function is calle 
+      body: jsonEncode(
+          operation)); //Now here comes the crucial part where we write operation like search for username, insert user data, etc. whatever operations you would perform would be placed in the operations section
+  var result = jsonDecode(response
+      .body); //Now we have gotten the result after waiting for the data, we then decode the data and also put it in a variable called result
+  return result; //This is returned whenever the function is calle
 }
